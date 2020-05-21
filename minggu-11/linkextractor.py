c904c749771d057a4167a07283b52674486217b8
putriute <56296325+putriute@users.noreply.github.com>
2020-05-21 20:56:23 +0700

    Add files via upload

---
+++ minggu-11/linkextractor.py
@@ -0,0 +1,8 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get(sys.argv[-1])
+soup = BeautifulSoup(res.text, "html.parser")
+for link in soup.find_all("a"):
+    print(link.get("href"))
